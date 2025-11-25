e4731a9f8bf5b0d6dc5b2c8333ed10968f113365
Doshtik <igor.lizyakinvladislavovic@gmail.com>
2025-11-25 19:02:09 +0500

    make some things

---
+++ app/src/main/java/com/lizyakin/mobile_amethyst_audio/adapters/AlbumItemAdapter.kt
@@ -0,0 +1,24 @@
+package com.lizyakin.mobile_amethyst_audio.adapters
+
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.lizyakin.mobile_amethyst_audio.adapters.AlbumItemAdapter.AlbumItemViewHolder
+
+class AlbumItemAdapter : RecyclerView.Adapter<AlbumItemViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): AlbumItemViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: AlbumItemViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    class AlbumItemViewHolder(val rootView: TextView) : RecyclerView.ViewHolder(rootView){
+
+    }
+}
---
+++ app/src/main/java/com/lizyakin/mobile_amethyst_audio/adapters/TrackItemAdapter.kt
@@ -0,0 +1,23 @@
+package com.lizyakin.mobile_amethyst_audio.adapters
+
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class TrackItemAdapter : RecyclerView.Adapter<TrackItemAdapter.TrackItemViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TrackItemViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: TrackItemViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    class TrackItemViewHolder(val rootView: TextView) : RecyclerView.ViewHolder(rootView){
+
+    }
+}
